d499c64636d386c4619b9f39edf97cb9963c73e6
Muhammad Iqbal <hellofromiqbal@gmail.com>
2024-02-08 12:08:30 +0700

    add express middleware size limit

--- index.js
+++ index.js
@@ -10,7 +10,8 @@ const PORT = process.env.PORT;
 const MONGODB_URI = process.env.MONGODB_URI;
 
 const app = express();
-app.use(express.json());
+app.use(express.json({ limit: '50mb' }));
+app.use(express.urlencoded({ limit: "50mb", extended: true }));
 app.use(cors());
 
 app.use("/books", bookRoutes);
